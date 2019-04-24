0a09999a5f1e2fde935a33d0bd8de7caa8686ec5
CrisRod <crodriguez85@gmail.com>
2019-04-24 09:55:03 -0400

    Wrapping ionic in folder

---
+++ ionic-js/app-course.js
@@ -0,0 +1,39 @@
+const courseName = document.querySelector('#input-name');
+const courseRating = document.querySelector('#input-rating');
+const addBtn = document.querySelector('#btn-confirm');
+const alert = document.querySelector('ion-alert-controller');
+const courseList = document.querySelector('#course-list');
+
+const clear = () => {
+    courseName.value = '';
+    courseRating.value = '';
+}
+
+addBtn.addEventListener('click', () => {
+    // console.log('Confirm Button Pressed');
+    const enteredName = courseName.value;
+    const enteredRating = courseRating.value;
+    // console.log(enteredReason);
+    // console.log(enteredAmount);
+
+    if(
+        enteredName.trim().length <= 0 ||
+        enteredRating <= 0 ||
+        enteredRating > 5 ||
+        enteredRating.trim().length <0
+    ) {
+        alert.create({
+            message: 'Please enter a valid reason and amount', 
+            header: 'Invalid Input',
+            buttons: ['Okay']
+        }).then(alertElement => {
+            alertElement.present();
+        });
+        return;
+    }
+     const newItem = document.createElement('ion-item');
+     newItem.innerHTML = `<strong>${enteredName}:</strong>&nbsp ${enteredRating}/5`
+     courseList.appendChild(newItem);
+
+    clear();
+})
